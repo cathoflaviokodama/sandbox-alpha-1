da562be71873a768dee76edbb41020a0cfa6ae8f
Flavio Kodama <flavio.kodama@catho.com>
2019-09-11 10:33:47 -0300

    init

--- index.js
+++ index.js
@@ -25,22 +25,50 @@ var sandbox = {
         app.shell = false;
     },
     routes : {
+        helloString : "OK",
+        helloFunction : () => { res.end("helloFunction"); },
+        helloObject : { a: 1 }
     }
 };
 server.get("/*",(req,res) => {
     var parts = req.url.split('?');
-    
-    res.end(req.url);
+    var found = true;
+    if(parts.length>0) {
+        var base_parts = parts[0].split("/");
+        var p = sandbox.routes;
+        for(var x = 0; x < base_parts.length;x++) {
+            console.log(base_parts[x]);
+            if( base_parts[x] in p ) {
+                
+                p = p[ base_parts[x] ];
+            } else {
+                found = false;
+                break;
+            }
+        }
+        if(found) {
+            var type = Object.prototype.toString.apply(p);
+            switch(type) {
+                case "[object Number]":
+                case "[object String]":
+                    res.end(p);
+                    break;
+                case "[object Function]":
+                    p(req,res);
+                    break;
+                case "[object Object]":
+                    res.json(p);
+                    break;
+
+            }
+        }
+    }
+    if(!found) res.status(404).end(req.url);
 });
 app.server = server.listen(9090);
-process.on('SIGTERM', () => {
-    rl.close();
-    app.server.close();
-});
-process.on('SIGINT', () => {
-    rl.close();
-    app.server.close();
-});
+app.quit = function() { rl.close(); app.server.close(); };
+process.on('SIGTERM', () => { app.quit(); });
+process.on('SIGINT', () => { app.quit(); });
 function shell() {
     rl.question('>', (answer) => {
         try {
@@ -51,10 +79,7 @@ function shell() {
             console.log(e);
         }
         if(app.shell) setTimeout(()=> { shell(); },0);
-        else {
-            app.server.close();
-            rl.close();
-        }
+        else app.quit();
     });
 }
 shell();
